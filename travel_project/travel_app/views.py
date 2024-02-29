eede5f6cb9320cf3da2fb9f54d689f755938b7b2
Vaniya S <124914359+VaniyaSharma@users.noreply.github.com>
2024-02-29 18:08:19 -0500

    fixing issues in the views and urls files

--- travel_project/travel_app/views.py
+++ travel_project/travel_app/views.py
@@ -10,6 +10,41 @@ def index(request):
     return render(request,'index.html')
 
 def user_sign_up(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        password = request.POST['pass']
+        confirmPass = request.POST['confirmPass']
+
+        if User.objects.filter(username=username):
+            messages.error(request, "Username already exist! Please try some other username.")
+            return redirect('sign-up')
+
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "Email Already Registered!!")
+            return redirect('sign-up')
+
+        if len(username) > 20:
+            messages.error(request, "Username must be under 20 charcters!!")
+            return redirect('sign-up')
+
+        if password != confirmPass:
+            messages.error(request, "Passwords didn't matched!!")
+            return redirect('sign-up')
+
+        if not username.isalnum():
+            messages.error(request, "Username must be Alpha-Numeric!!")
+            return redirect('sign-up')
+
+        newuser = User.objects.create_user(username, email, password)
+        newuser.first_name = first_name
+        newuser.last_name = last_name
+        newuser.is_active = False
+        newuser.save()
+
+    return render(request, "registration/sign-up.html")
 
 def user_login(request):
     if request.method == 'POST':
