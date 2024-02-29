6ec8adfaff8a1bfaa72b832165185f3a0482f491
Vaniya S <124914359+VaniyaSharma@users.noreply.github.com>
2024-02-29 14:31:09 -0500

    created django login page

--- travel_project/travel_app/urls.py
+++ travel_project/travel_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import index
+from . import views
 
 urlpatterns = [
     path('', index, name='index'),
+    path('login/', views.user_login, name='login'),
 ]
--- travel_project/travel_app/views.py
+++ travel_project/travel_app/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
 def index(request):
     return render(request,'index.html')
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['pass']
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            first_name = user.first_name
+
+            return render(request, "index.html", {"first_name": first_name})
+        else:
+            messages.error(request, "Wrong Credentials!!")
+            return redirect('home')
+
+    return render(request, "registration/login.html")
