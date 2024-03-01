4957f4fb41b5c583215eaabdd14ad26c166ccf79
rashaibr <96967976+rashaibr@users.noreply.github.com>
2024-03-01 12:04:10 -0500

    Added style

--- travel_project/travel_app/urls.py
+++ travel_project/travel_app/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import index
+from . import views
 
 urlpatterns = [
     path('', index, name='index'),
+    path('sign-up/', views.user_sign_up, name='sign-up'),
+    path('login/', views.user_login, name='login'),
+
 ]
+
