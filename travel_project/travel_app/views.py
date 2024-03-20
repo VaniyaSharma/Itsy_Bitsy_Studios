8abe8239c47120af6f2d988d93902303410d40d3
Vaniya S <124914359+VaniyaSharma@users.noreply.github.com>
2024-03-19 21:15:48 -0400

    added activation email template
    added template file for email  activation of user account, updates the models and  views

--- travel_project/travel_app/views.py
+++ travel_project/travel_app/views.py
@@ -1,13 +1,53 @@
+import threading
+
+from Tools.scripts import generate_token
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
+
+from Itsy_Bitsy_Studios.travel_project.travel_project import settings
+
 
 # Create your views here.
 
 def index(request):
     return render(request,'index.html')
+
+
+class EmailThread(threading.Thread):
+
+    def __init__(self, email):
+        self.email = email
+        threading.Thread.__init__(self)
+
+    def run(self):
+        self.email.send()
+
+
+def send_activation_email(user, request):
+    current_page = get_current_site(request)
+    email_sub = 'Activate your account'
+    email_content = render_to_string('registration/activate_email.html', {
+        'user': user,
+        'domain': current_page,
+        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+        'token': generate_token.make_token(user)
+    })
+
+    email = EmailMessage(subject=email_sub, body=email_content,
+                         from_email=settings.EMAIL_FROM_USER,
+                         to=[user.email]
+                         )
+
+    if not settings.TESTING:
+        EmailThread(email).start()
 
 def user_sign_up(request):
     if request.method == "POST":
@@ -44,6 +84,12 @@ def user_sign_up(request):
         newuser.is_active = False
         newuser.save()
 
+        if not messages.error['has_error']:
+            send_activation_email(user, request)
+
+            messages.add_message(request, messages.SUCCESS,
+                                 'We sent you an email to verify your account')
+
         return redirect('login')
 
     return render(request, "registration/sign-up.html")
@@ -54,6 +100,11 @@ def user_login(request):
         password = request.POST['pass']
 
         user = authenticate(username=username, password=password)
+
+        if user and not user.is_email_verified:
+            messages.add_message(request, messages.ERROR,
+                                 'Email is not verified, please check your email inbox')
+            return render(request, 'registration/login.html')
 
         if user is not None:
             login(request, user)
