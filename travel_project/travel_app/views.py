00ef9c29e3064d22a5db3bf663473da2716f6cf1
Vaniya S <124914359+VaniyaSharma@users.noreply.github.com>
2024-03-22 22:44:14 -0400

    user authentication and email confirmation

--- travel_project/travel_app/views.py
+++ travel_project/travel_app/views.py
@@ -1,3 +1,4 @@
+from django.core.mail import send_mail
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
@@ -5,14 +6,17 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 
 
+
 from django.db import models
+
+from travel_project import settings
+
 
 # Create your models here.
 
 
 def index(request):
     return render(request,'index.html')
-
 
 
 
@@ -23,28 +27,28 @@ def user_sign_up(request):
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
         email = request.POST['email']
-        password = request.POST['pass']
+        password = request.POST['password']
         confirmPass = request.POST['confirmPass']
 
         if User.objects.filter(username=username):
             messages.error(request, "Username already exist! Please try some other username.")
-            return redirect('sign-up')
+            return redirect('index')
 
         if User.objects.filter(email=email).exists():
             messages.error(request, "Email Already Registered!!")
-            return redirect('sign-up')
+            return redirect('index')
 
         if len(username) > 20:
             messages.error(request, "Username must be under 20 charcters!!")
-            return redirect('sign-up')
+            return redirect('index')
 
         if password != confirmPass:
             messages.error(request, "Passwords didn't matched!!")
-            return redirect('sign-up')
+            return redirect('index')
 
         if not username.isalnum():
             messages.error(request, "Username must be Alpha-Numeric!!")
-            return redirect('sign-up')
+            return redirect('index')
 
         newuser = User.objects.create_user(username, email, password)
         newuser.first_name = first_name
@@ -52,7 +56,18 @@ def user_sign_up(request):
         newuser.is_active = False
         newuser.save()
 
+        print("New User:", newuser.username, newuser.email, newuser.password)
+
         messages.success(request,"Your account has been succesfully created!")
+
+        # Welcome email
+
+        subject = "Welcome to Tripsee!!"
+        message= "Hello" + newuser.first_name + "!\n" + "Thank you for registering on our website. We are so excited to have you with us. \n" + "Please check your email for account confirmation to login and start planning your trip! \n\n" + "Thank you.\n With regards, \n Tripsee team"
+        from_email = settings.EMAIL_HOST_USER
+
+        to_user = [newuser.email]
+        send_mail(subject, message, from_email, to_user, fail_silently=True)
 
 
 
@@ -63,10 +78,18 @@ def user_sign_up(request):
 def user_login(request):
     if request.method == 'POST':
         username = request.POST['username']
-        password = request.POST['pass']
+        password = request.POST['password']
+
+        print("Username:", username)  # Debugging statement
+        print("Password:", password)  # Debugging statement
 
         user = authenticate(username=username, password=password)
+        
+        user = User.objects.filter(username=username).first()
+        if user is None:
+            print("User does not exist:", username)
 
+        print("Authenticated User:", user)
 
         if user is not None:
             login(request, user)
