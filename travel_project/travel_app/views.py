d3f7dd19739f2e3aba21becceece91629563287a
Vaniya S <124914359+VaniyaSharma@users.noreply.github.com>
2024-03-23 23:29:33 -0400

    Working user authentication finished

--- travel_project/travel_app/views.py
+++ travel_project/travel_app/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserForm
 
+from django.contrib.auth.decorators import login_required
+
 def index(request):
     return render(request, 'index.html')
 
@@ -17,6 +19,10 @@ def user_sign_up(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+
+            return redirect('login')
 
     context = {'form': form}
 
@@ -24,13 +30,25 @@ def user_sign_up(request):
 
 
 def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.info(request, 'Username or password is incorrect')
+            return redirect('login')
 
     return render(request, "registration/login.html")
 
 
 def user_logout(request):
     logout(request)
-    return redirect('index')
+    return redirect('login')
 
 
 def contact_us(request):
@@ -45,5 +63,6 @@ def search(request):
     return render(request, 'search.html')
 
 
+# login_required(login_url='login')
 def itinerary(request):
     return render(request, 'itinerary.html')
